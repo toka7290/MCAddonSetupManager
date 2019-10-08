1f82b8526936554a140ee2557a9c76c931ab0a04
toka7290 <tokaiko7290@gmail.com>
2019-10-08 19:43:47 +0900

    v2

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MCAddonSetupManager.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,15 @@ namespace MCAddonSetupManager
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var w = new MainWindow();
+            var vm = new MainWindowViewModel();
+
+            w.DataContext = vm;
+            w.Show();
+        }
     }
 }
---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCAddonSetupManager.ViewModels
+{
+    class MainWindowViewModel : NotificationObject
+    {
+        private string text1;
+        public string Text1
+        {
+            get { return text1; }
+            set { SetProperty(ref text1, value); }
+        }
+    }
+}
