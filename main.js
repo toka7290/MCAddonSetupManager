2caac1ebd2e80055f7611a6ee8c0e3d75aa3577e
とかさん <46467578+toka7290@users.noreply.github.com>
2020-04-28 15:16:38 +0900

    20200428

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu, MenuItem, dialog } = require('electron')
+var path = require('path');
+
+// The default value of app.allowRendererProcessReuse is deprecated, it is currently "false".  It will change to be "true" in Electron 9.  For more information please check https://github.com/electron/electron/issues/18397
+app.allowRendererProcessReuse = true
 
 // ウインドウオブジェクトのグローバル参照を保持してください。さもないと、そのウインドウは
 // JavaScript オブジェクトがガベージコレクションを行った時に自動的に閉じられます。
@@ -12,14 +16,14 @@ function createWindow () {
     height: 600,
     webPreferences: {
       nodeIntegration: true
-    }
-  })
+    },
+    icon: 'img/Material_icon_v2.png'
+  });
+
+  setMenu();
 
   // そしてこのアプリの index.html をロード
-  win.loadFile('index.html')
-
-  // 開発者ツールを開く
-  //win.webContents.openDevTools()
+  win.loadFile('index.html');
 
   // ウィンドウが閉じられた時に発火
   win.on('closed', () => {
@@ -28,13 +32,13 @@ function createWindow () {
     // 配列にウインドウを格納する。
     // ここは該当する要素を削除するタイミング。
     win = null
-  })
+  });
 }
 
 // このイベントは、Electronが初期化処理と
 // browser windowの作成を完了した時に呼び出されます。
 // 一部のAPIはこのイベントが発生した後にのみ利用できます。
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // 全てのウィンドウが閉じられた時に終了する
 app.on('window-all-closed', () => {
@@ -43,7 +47,7 @@ app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+});
 
 app.on('activate', () => {
   // macOSでは、ユーザがドックアイコンをクリックしたとき、
@@ -51,8 +55,36 @@ app.on('activate', () => {
   if (win === null) {
     createWindow()
   }
-})
+});
 
 // このファイル内には、
 // 残りのアプリ固有のメインプロセスコードを含めることができます。 
 // 別々のファイルに分割してここで require することもできます。
+
+//メニュー
+function setMenu(){
+  const menu = new Menu()
+  const menuItemFile = [
+    new MenuItem({label: 'Exit(&E)', role: 'quit'})
+  ];
+  menu.append(new MenuItem({ label: 'File(&F)', submenu: menuItemFile }));
+  const menuItemAbout = [
+    new MenuItem({label: 'Debag(&D)', click: () => {win.webContents.openDevTools()}}),
+    new MenuItem({label: 'About(&A)', click: showAbout})
+  ];
+  menu.append(new MenuItem({ label: 'Help(&H)', submenu: menuItemAbout }));
+  Menu.setApplicationMenu(menu);
+}
+
+function showAbout(){
+  chromeVersion = new String("Chrome: "+ process.versions.chrome + "\n") ;
+  electronVersion = new String("Electron: "+ process.versions.electron + "\n") ;
+  dialog.showMessageBox(
+    { 
+      type: "info",
+      title: path.basename(process.execPath,path.extname(process.execPath)),
+      message: "MCaddonSetupManager",
+      detail: chromeVersion+electronVersion
+    }
+  );
+}
