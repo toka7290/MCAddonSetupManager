8cb4726a883fb7d3c8d7599ca20ddcf9a3dfe0cf
とかさん <46467578+toka7290@users.noreply.github.com>
2020-05-08 16:56:09 +0900

    05081656

--- serviceworker.js
+++ serviceworker.js
@@ -45,14 +45,3 @@ self.addEventListener('fetch', function(event) {
             })
     );
 });
-
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the mini-infobar from appearing on mobile
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can install the PWA
-  showInstallPromotion();
-});
